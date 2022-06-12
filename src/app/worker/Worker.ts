22a1edf5fb379c30b792932848dfccf6b0aa6dcc
Justin Head <jhead@jxh.io>
2022-06-11 20:04:51 -0700

    spew button

--- src/app/worker/Worker.ts
+++ src/app/worker/Worker.ts
@@ -12,9 +12,6 @@ const startEngine = async () => {
   const stateStore = await StateDB.create()
   const engine = new Engine(stateStore)
 
-  // TODO
-  createTestEntities(engine)
-
   engine.onEngineEvent((event) => {
     postMessage(event)
   })
@@ -26,26 +23,6 @@ const startEngine = async () => {
 
   engine.start()
   console.log('Engine started!')
-}
-
-const createTestEntities = (engine: Engine) => {
-  setInterval(() => {
-    const size = Scale.Static(5 + Math.random() * 25)
-
-    engine.sendCommand({
-      type: 'PutEntity',
-      entity: new Square({
-        size: {
-          width: size,
-          height: size,
-        },
-        velocity: {
-          x: Math.random() * 3,
-          y: Math.random() * 3,
-        },
-      }),
-    })
-  }, 50)
 }
 
 startEngine()
--- src/ui/editor/Editor.tsx
+++ src/ui/editor/Editor.tsx
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { EntityCommand } from '../../api/event/EngineEventAPI'
 import { Square } from '../../api/obj/Square'
+import { Scale } from '../../api/types/Geom'
 import { DekuContext } from '../context/DekuContext'
 
 export const Editor: React.FC = () => {
   return (
     <>
-      <TestButton />
+      <AddObjectButton />
+      <SpewButton />
       <ResetStateButton />
     </>
   )
@@ -22,7 +24,7 @@ const ResetStateButton: React.FC = () => {
   return <button onClick={onClick}>Reset</button>
 }
 
-const TestButton: React.FC = () => {
+const AddObjectButton: React.FC = () => {
   const ctx = useContext(DekuContext)
 
   const onClick = () => {
@@ -37,5 +39,40 @@ const TestButton: React.FC = () => {
     })
   }
 
-  return <button onClick={onClick}>Test</button>
+  return <button onClick={onClick}>Add</button>
+}
+
+const SpewButton: React.FC = () => {
+  const ctx = useContext(DekuContext)
+  const [interval, saveInterval] = useState<number>()
+  const enabled = typeof interval !== 'undefined'
+
+  const createTestEntity = () => {
+    const size = Scale.Static(5 + Math.random() * 25)
+
+    ctx.eventing.emit<EntityCommand.PutEntity>('Worker', {
+      type: 'PutEntity',
+      entity: new Square({
+        size: {
+          width: size,
+          height: size,
+        },
+        velocity: {
+          x: Math.random() * 3,
+          y: Math.random() * 3,
+        },
+      }),
+    })
+  }
+
+  const onClick = () => {
+    if (enabled) {
+      clearInterval(interval)
+      saveInterval(undefined)
+    } else {
+      saveInterval(setInterval(createTestEntity, 50))
+    }
+  }
+
+  return <button onClick={onClick}>{!enabled ? 'Spew' : 'Unspew'}</button>
 }
